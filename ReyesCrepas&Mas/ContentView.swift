5b2eb43988d403ac4f8deebede46babf4268dfbd
JoanMay132 <jcmd270@gmail.com>
2024-12-20 14:28:21 -0600

    Initial Commit

---
+++ ReyesCrepas&Mas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReyesCrepas&Mas
+//
+//  Created by Joan May on 20/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReyesCrepas&Mas/ReyesCrepas_MasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReyesCrepas_MasApp.swift
+//  ReyesCrepas&Mas
+//
+//  Created by Joan May on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ReyesCrepas_MasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
