7f59d254fcb2befccbb7292c6030b1c301da1922
JoanMay132 <jcmd270@gmail.com>
2024-12-21 22:18:56 -0600

    [Feature] Adding products to Home Page Menu.

--- ReyesCrepas&Mas/Frappe.swift
+++ ReyesCrepas&Mas/Frappe.swift
@@ -9,14 +9,17 @@ import Foundation
 
 
 struct Frappes: Identifiable, Codable {
+    
+    struct Extra: Identifiable, Codable {
+        let id: Int
+        let name: String
+        let price: String
+    }
+    
     let id: String
     let name: String
     let price: String
     let extras: [Extra]
 }
 
-struct Extra: Identifiable, Codable {
-    let id: Int?
-    let name: String
-    let price: String?
-}
+
